6646a0622dff48c90c6652f787695fa19fd2e882
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-21 19:55:22 +0430

    Add the webpage layout.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-// import {
-//   BrowserRouter, Routes, Route, Outlet, Link
-// } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.scss';
+import Home from './components/Home';
 import Quote from './components/Quote';
 import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="app">
-      <Quote />
-      <Calculator />
-    </div>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Calculator</Link>
+            </li>
+            <li>
+              <Link to="/quote">Quote</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/calculator" element={<Calculator />} />
+        </Routes>
+      </main>
+      <footer>
+        <p>&copy; 2023 Math Magicians | Made by Abdulali Noori</p>
+      </footer>
+    </>
   );
 }
 
